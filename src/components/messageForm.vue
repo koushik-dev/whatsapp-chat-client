fe815ee5475dc9ebdaa03781a81878f94d151f5a
koushik <koushik.ramaswamy@apitive.com>
2020-06-23 10:20:03 +0530

    vibration added

--- src/components/messageForm.vue
+++ src/components/messageForm.vue
@@ -29,6 +29,7 @@ export default {
   },
   methods: {
     submit() {
+      window.navigator.vibrate([100,30,100,30,100,30,200,30,200,30,200,30,100,30,100,30,100]);
       let input = document.querySelector(".chat__msg");
       this.$socket.emit("chat-message", {
         msg: input.value,
